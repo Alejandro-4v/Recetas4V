51084092d40a22ca0f2b21dd40db5fe4b8a23134
Alejacks <alejandrolopez2048@gmail.com>
2025-11-07 19:00:28 +0100

    feat: Molécula de recipe-card creada

--- recetas/src/app/molecules/recipe-card/recipe-card.ts
+++ recetas/src/app/molecules/recipe-card/recipe-card.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '@models/recipe.model'; // ¡Importamos la interfaz!
+import { ButtonComponent } from '@atoms/button/button';
 
 @Component({
   selector: 'app-recipe-card',
-  imports: [],
   templateUrl: './recipe-card.html',
-  styles: ``,
+  imports: [ButtonComponent],
+  standalone: true
 })
-export class RecipeCard {
+export class RecipeCardComponent {
+  @Input() recipe!: Recipe; // Uso ! para asegurar que nunca será nulo
 
+  @Output() deleteRequest = new EventEmitter<number>(); 
+
+  onDelete(): void {
+    // Verificamos que la receta exista y emitimos su ID.
+    if (this.recipe) {
+      this.deleteRequest.emit(this.recipe.id);
+    }
+  }
 }
