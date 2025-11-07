0938401af72a9af9e05fbdaeed66bff68706fba6
Alejacks <alejandrolopez2048@gmail.com>
2025-11-07 20:22:20 +0100

    feat: Crear molécula de item de formulario

--- src/app/molecules/form-group/form-group.ts
+++ src/app/molecules/form-group/form-group.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputFieldComponent } from '@atoms/input-field/input-field';
+
 
 @Component({
   selector: 'app-form-group',
-  imports: [],
   templateUrl: './form-group.html',
-  styles: ``,
+  imports: [
+    CommonModule,
+    InputFieldComponent,
+  ],
+  standalone: true
 })
-export class FormGroup {
+export class FormGroupComponent {
+  @Input() label: string = ''; 
 
+  @Input() type: string = 'text';
+
+  @Input() placeholder: string = '';
+
+  @Input() value: string | number = '';
+
+  @Output() valueChange = new EventEmitter<string | number>();
+
+  // Captura el evento valueChange del InputField Átomo y lo reemite 
+  handleValueChange(newValue: string | number): void {
+    // Reemitimos el evento hacia el organismo/página
+    this.valueChange.emit(newValue);
+  }
 }
