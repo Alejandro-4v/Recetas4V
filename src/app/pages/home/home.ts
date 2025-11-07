b6f048267d3f4a99b861cb983aa5a0e156a8ac6b
Alejacks <alejandrolopez2048@gmail.com>
2025-11-07 20:23:29 +0100

    feat: Lógica en home para guardar array de recetas creado y componente añadido a app-root

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Recipe } from '@models/recipe.model';
+import { DEFAULT_RECIPES } from 'src/app/default-recipes';
+import { RecipeListComponent } from '../../organisms/recipe-list/recipe-list';
+import { RecipeFormComponent } from '../../organisms/recipe-form/recipe-form';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
   templateUrl: './home.html',
-  styles: ``,
+  imports: [
+    CommonModule, 
+    RecipeListComponent,
+    RecipeFormComponent
+  ]
 })
-export class Home {
+export class HomeComponent implements OnInit {
+  public recipes: Recipe[] = []; 
 
+  constructor() { }
+
+  // Cargo recetas
+  ngOnInit(): void {
+    // Con ese triple punto hago una copia del array
+    this.recipes = [...DEFAULT_RECIPES]; 
+  }
+
+
+  addRecipe(newRecipe: Recipe): void {
+    // Creo copia del array y añado la nueva receta.
+    this.recipes = [...this.recipes, newRecipe];
+    console.log('Receta añadida. Total:', this.recipes.length);
+  }
+
+  // Borro receta haciendo una copia del array y guardando todas menos la que coincide con el ID a borrar
+  deleteRecipe(id: number): void {
+    this.recipes = this.recipes.filter(recipe => recipe.id !== id);
+    console.log(`Receta con ID ${id} eliminada. Total restante:`, this.recipes.length);
+  }
 }
