8d24caf5242bdb8f9d53870d723115f9c2eaed17
Alejacks <alejandrolopez2048@gmail.com>
2025-11-07 18:23:32 +0100

    feat: Implementar Átomos Button e InputField con la estructura correcta

--- recetas/src/app/atoms/button/button.ts
+++ recetas/src/app/atoms/button/button.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  imports: [],
   templateUrl: './button.html',
-  styles: ``,
 })
-export class Button {
+export class ButtonComponent {
+  @Input() text: string = 'Botón'; 
 
+  @Input() colorClass: string = 'btn-primary';
+
+  @Output() clickEvent = new EventEmitter<void>();
+
+  handleClick(): void {
+    this.clickEvent.emit();
+  }
 }
--- recetas/src/app/atoms/input-field/input-field.ts
+++ recetas/src/app/atoms/input-field/input-field.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-input-field',
-  imports: [],
   templateUrl: './input-field.html',
-  styles: ``,
 })
-export class InputField {
+export class InputFieldComponent {
+  @Input() type: string = 'text';
 
+  @Input() placeholder: string = 'placeholder';
+
+  @Input() value: string | number = '';
+
+  @Output() valueChange = new EventEmitter<string | number>();
+
+  onInputChange(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    this.valueChange.emit(inputElement.value);
+  }
 }
