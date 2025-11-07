4b1e7b3f234796ef3bbef724a2360f8ec289e6bc
Alejacks <alejandrolopez2048@gmail.com>
2025-11-07 19:23:17 +0100

    feat: Crear Organismo RecipeList, responsable de mostrar la lista de RecipeCard y reemitir la solicitud de eliminación.

--- src/app/organisms/recipe-list/recipe-list.ts
+++ src/app/organisms/recipe-list/recipe-list.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Recipe } from '@models/recipe.model';
+import { RecipeCardComponent } from '../../molecules/recipe-card/recipe-card';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [],
+  standalone: true,
   templateUrl: './recipe-list.html',
-  styles: ``,
+  imports: [
+    CommonModule, RecipeCardComponent
+  ]
 })
-export class RecipeList {
+export class RecipeListComponent {
+  // El array de recetas que este organismo recibe del padre
+  @Input() recipes: Recipe[] = [];
 
+  // Evento para eliminar receta (enviado al padre)
+  @Output() deleteRequest = new EventEmitter<number>();
+
+  handleDeleteRequest(recipeId: number): void {
+    // Envio el ID. La lógica de eliminación real estará en el componente de la página.
+    this.deleteRequest.emit(recipeId);
+  }
 }
